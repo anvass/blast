be715daa4021f0f31c26aab82fc6e0ccbad45755
anvass <nastya_0809@mail.ru>
2025-06-10 16:44:15 +0300

    feat: output of both sequences and coloring of the first one

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,22 @@
-import { Box, Button, Container, Grid, TextField } from '@mui/material';
+import {
+  Box,
+  Button,
+  Container,
+  Divider,
+  Grid,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 interface IFormInput {
   firstSeqValue: string;
   secondSeqValue: string;
+}
+
+interface IColorTable {
+  [key: string]: string;
 }
 
 function Form() {
@@ -12,6 +25,7 @@ function Form() {
     register,
     formState: { errors },
     watch,
+    reset,
   } = useForm<IFormInput>({
     defaultValues: {
       firstSeqValue: '',
@@ -22,8 +36,12 @@ function Form() {
 
   const firstSeqFieldValue = watch('firstSeqValue');
 
+  const [submittedData, setSubmittedData] = useState<IFormInput | null>(null);
+
   const onSubmit = (data: IFormInput) => {
     console.log('Form data:', data);
+    setSubmittedData(data);
+    reset();
   };
 
   const validatePattern = (value: string) => {
@@ -40,6 +58,29 @@ function Form() {
     );
   };
 
+  const colorTable: IColorTable = {
+    C: '#FFEA00',
+    A: '#67E4A6',
+    I: '#67E4A6',
+    L: '#67E4A6',
+    M: '#67E4A6',
+    F: '#67E4A6',
+    W: '#67E4A6',
+    Y: '#67E4A6',
+    V: '#67E4A6',
+    P: '#67E4A6',
+    G: '#C4C4C4',
+    D: '#FC9CAC',
+    E: '#FC9CAC',
+    K: '#BB99FF',
+    R: '#BB99FF',
+    S: '#80BFFF',
+    T: '#80BFFF',
+    H: '#80BFFF',
+    Q: '#80BFFF',
+    N: '#80BFFF',
+  };
+
   return (
     <Container sx={{ my: '10vh' }}>
       <Box component="form" onSubmit={handleSubmit(onSubmit)}>
@@ -49,12 +90,6 @@ function Form() {
               {...register('firstSeqValue', {
                 required: 'This field is required',
                 validate: (value) => validatePattern(value) || true,
-                // validate: (value) => {
-                //   validatePattern(value);
-                //   const patternError = validatePattern(value);
-                //   if (patternError !== true) return patternError;
-                //   return validateSameLength(value, secondSeqFieldValue) || true;
-                // },
               })}
               id="firstSeqValue"
               label="First amino sequence"
@@ -96,6 +131,47 @@ function Form() {
           </Grid>
         </Grid>
       </Box>
+
+      <Divider sx={{ my: '5vh' }} />
+
+      {submittedData && (
+        <Box component="div" sx={{ p: 2, border: '1px dashed grey' }}>
+          <Grid container spacing={2}>
+            <Grid size={{ xs: 12 }}>
+              <Typography
+                variant="overline"
+                gutterBottom
+                component="p"
+                sx={{ fontSize: '1.1em', lineHeight: 'inherit' }}
+              >
+                {submittedData.firstSeqValue.split('').map((char, index) => (
+                  <Typography
+                    component="span"
+                    key={index}
+                    sx={{
+                      backgroundColor: colorTable[char] || '#000000',
+                      fontSize: 'inherit',
+                      letterSpacing: 'inherit',
+                      display: 'inline-block',
+                    }}
+                  >
+                    {char}
+                  </Typography>
+                ))}
+              </Typography>
+            </Grid>
+            <Grid size={{ xs: 12, md: 8 }}>
+              <Typography
+                variant="overline"
+                component="p"
+                sx={{ fontSize: '1.1em', lineHeight: 'inherit' }}
+              >
+                {submittedData.secondSeqValue}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Box>
+      )}
     </Container>
   );
 }
