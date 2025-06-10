0848a9b7d5bb67d095e08f9ee24bc2d677e68c1e
anvass <nastya_0809@mail.ru>
2025-06-10 14:19:29 +0300

    feat: form fields validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,40 +1,89 @@
 import { Button, Container, Grid, TextField } from '@mui/material';
+import { useForm } from 'react-hook-form';
+
+interface IFormInput {
+  firstSeqValue: string;
+  secondSeqValue: string;
+}
 
 function Form() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    watch,
+  } = useForm<IFormInput>({
+    // defaultValues: {
+    //   firstSeqValue: '',
+    //   secondSeqValue: '',
+    // },
+    mode: 'onChange',
+  });
+
+  const firstSeqFieldValue = watch('firstSeqValue');
+  const secondSeqFieldValue = watch('secondSeqValue');
+  console.log('1', firstSeqFieldValue);
+  console.log('2', secondSeqFieldValue);
+
+  const onSubmit = (data: IFormInput) => {
+    console.log('Form data:', data);
+  };
+
+  const validatePattern = (value: string) => {
+    if (!/^[ARNDCEQGHILKMFPSTWY\-V-]+$/.test(value)) {
+      return 'The field can only contain Latin letters of amino acids and the dash symbol';
+    }
+    return true;
+  };
+
+  const validateSameLength = (value1: string, value2: string) => {
+    return (
+      value1.length === value2.length ||
+      'Length of the entered sequences in BOTH fields must be the same'
+    );
+  };
+
   return (
     <Container sx={{ my: '10vh' }}>
-      <form
-      //   {...form.handleSubmit(onSubmit)}
-      >
+      <form {...handleSubmit(onSubmit)}>
         <Grid container spacing={2} sx={{ justifyContent: 'center' }}>
           <Grid size={{ xs: 12, md: 8 }}>
             <TextField
-              // {...form.register('field1', {
-              //   required: 'Это поле обязательное',
-              //   validate: (value) => validateLatinLetters(value) || true,
-              // })}
+              {...register('firstSeqValue', {
+                required: 'This field is required',
+                validate: (value) => validatePattern(value) || true,
+                // validate: (value) => {
+                //   validatePattern(value);
+                //   const patternError = validatePattern(value);
+                //   if (patternError !== true) return patternError;
+                //   return validateSameLength(value, secondSeqFieldValue) || true;
+                // },
+              })}
+              id="firstSeqValue"
               label="First amino sequence"
               variant="outlined"
               fullWidth
-              // error={!!form.formState.errors.field1}
-              // helperText={form.formState.errors.field1?.message}
+              error={!!errors.firstSeqValue}
+              helperText={errors.firstSeqValue?.message}
             />
           </Grid>
           <Grid size={{ xs: 12, md: 8 }}>
             <TextField
-              // {...form.register('field2', {
-              //   required: 'Это поле обязательное',
-              //   validate: (value) => {
-              //     const latinError = validateLatinLetters(value);
-              //     if (latinError !== true) return latinError;
-              //     return validateSameLength(value) || true;
-              //   },
-              // })}
+              {...register('secondSeqValue', {
+                required: 'This field is required',
+                validate: (value) => {
+                  validatePattern(value);
+                  const patternError = validatePattern(value);
+                  if (patternError !== true) return patternError;
+                  return validateSameLength(value, firstSeqFieldValue) || true;
+                },
+              })}
+              id="secondSeqValue"
               label="Second amino sequence"
               variant="outlined"
               fullWidth
-              // error={!!form.formState.errors.field2}
-              // helperText={form.formState.errors.field2?.message}
+              error={!!errors.secondSeqValue}
+              helperText={errors.secondSeqValue?.message}
             />
           </Grid>
           <Grid size={{ xs: 12, md: 8 }}>
