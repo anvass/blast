4704407dc7dc9c2a50376ee6c425a20af33da814
anvass <nastya_0809@mail.ru>
2025-06-11 13:12:25 +0300

    feat: logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,18 @@
-import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
-import BiotechTwoToneIcon from '@mui/icons-material/BiotechTwoTone';
+import { AppBar, Toolbar, Typography } from '@mui/material';
 
 function Header() {
   return (
     <>
       <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <BiotechTwoToneIcon />
-          </IconButton>
+        <Toolbar variant="dense" sx={{ py: 1 }}>
           <Typography
             variant="h6"
             noWrap
             component="a"
             href="/"
             sx={{
+              display: 'flex',
+              alignItems: 'center',
               mr: 2,
               fontFamily: 'monospace',
               fontWeight: 700,
@@ -28,7 +21,17 @@ function Header() {
               textDecoration: 'none',
             }}
           >
-            BLAST
+            <img
+              src={'/logo.svg'}
+              alt="BLAST"
+              style={{
+                width: '100%',
+                maxWidth: '50px',
+                height: 'auto',
+                marginRight: '10px',
+              }}
+            />
+            <span>BLAST</span>
           </Typography>
         </Toolbar>
       </AppBar>
