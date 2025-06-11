8f62da04053200527791ba8dc5c50aca122ef1f1
anvass <nastya_0809@mail.ru>
2025-06-11 16:44:47 +0300

    build: deployment

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -18,7 +18,7 @@ function Logo({
       }}
     >
       <img
-        src={'/logo.svg'}
+        src={`${import.meta.env.BASE_URL}/logo.svg`}
         alt="BLAST"
         style={{
           width: '100%',
